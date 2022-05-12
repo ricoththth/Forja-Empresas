d25fe44faabb7feaa27995e22c258bb2fff9d863
Lizeth Rico <ricogomezlauralizeth@gmail.com>
2022-05-11 21:17:37 -0500

    login fixed

--- js/login.js
+++ js/login.js
@@ -1,11 +1,13 @@
-function login(){
-    let user, pass
+function load(){
 
-    user = document.getElementsByClassName("Usuario")
-    pass = document.getElementsByClassName("Contraseña")
-
-    if(user == "Admin" && pass == "Kuepa2022"){
-       // location = "dashboard.html"
-        //window.location = "dashboard.html"
+    if (document.form.password.value=='Kuepa2022' && document.form.login.value=='Admin'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+            Swal.fire({
+                icon: 'error',
+                title: 'Tus datos estan erroneos :(',
+                text: 'Intenta de nuevo',
+            })
+        } 
     }
-}
